c9d29f2402946e3d8b92ae3cd76eb390b9b97254
mixei1902 <misei190289@gmail.com>
2024-03-19 18:37:41 +0300

    add API HH

---
+++ src/APIHeadHunter.py
@@ -0,0 +1,16 @@
+import requests
+from abc import ABC, abstractmethod
+
+class AbstractAPI(ABC):
+    @abstractmethod
+    def get_vacancies(self, search_query):
+        pass
+
+class HeadHunterAPI(AbstractAPI):
+    def __init__(self):
+        self.base_url = "https://api.hh.ru/vacancies"
+
+    def get_vacancies(self, search_query):
+        params = {"text": search_query, "area": 1}  # Здесь 1 - это код региона (Москва)
+        response = requests.get(self.base_url, params=params)
+        return response.json()
