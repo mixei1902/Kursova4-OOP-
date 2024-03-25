ca4e62f37ca1c3f77af4648f314e61db2bf1cca4
mixei1902 <misei190289@gmail.com>
2024-03-25 09:25:07 +0300

    fix filter

--- src/APIHeadHunter.py
+++ src/APIHeadHunter.py
@@ -1,10 +1,13 @@
-import requests
 from abc import ABC, abstractmethod
+
+import requests
+
 
 class AbstractAPI(ABC):
     @abstractmethod
     def get_vacancies(self, search_query):
         pass
+
 
 class HeadHunterAPI(AbstractAPI):
     def __init__(self):
